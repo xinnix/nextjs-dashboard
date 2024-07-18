fcf65fb5537813296bc283692db2c4298bd7c73b
辛昕 <xinnix@xinxindeMacBook-Pro.local>
2024-07-18 23:15:49 +0800

    opti

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: {
+    template: "%s | ACME Dashboard",
+    default: "ACME Dashboard",
+  },
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 export default function RootLayout({
   children,
 }: {
